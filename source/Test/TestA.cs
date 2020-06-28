0bb69269ac13e4cb14f92e9f57c619659248c96f
kazenetu <rakudafactory@gmail.com>
2020-06-29 08:07:38 +0900

    [WIP]テストコートを作成

---
+++ source/Test/TestA.cs
@@ -0,0 +1,47 @@
+using Domain.Domain.Entity;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text;
+using Xunit;
+
+namespace Test
+{
+  [Trait("テスト", nameof(ClassTest))]
+  public class TestA
+  {
+    [Fact]
+    public void TestSample()
+    {
+      var jsonText = @"
+[
+    {
+        ""number"": 2,
+        ""title"": ""イシュー２"",
+        ""user"": {
+            ""login"": ""user"",
+            ""email"": ""user@"",
+            ""type"": ""User"",
+            ""site_admin"": false,
+            ""created_at"": ""2020-06-05T08:59:57Z"",
+            ""id"": 0,
+            ""url"": ""url1"",
+            ""html_url"": ""url2"",
+            ""avatar_url"": ""url3""
+        },
+        ""labels"": [],
+        ""state"": ""open"",
+        ""created_at"": ""2020-06-09T04:32:49Z"",
+        ""updated_at"": ""2020-06-18T07:13:54Z"",
+        ""body"": ""イシュー２"",
+        ""id"": 0,
+        ""comments_url"": ""comments_url/issues/2/comments"",
+        ""html_url"": ""html_url/test/issues/2""
+    }
+]      
+      ";
+
+      var issues = JsonSerializer.Deserialize<List<IssueEntity>>(jsonText);
+
+      Assert.True(issues.count == 1);
+    }
+}
