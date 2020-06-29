77c2abc2bf483adb4bc2f20b4573ba3b796def80
kazenetu <rakudafactory@gmail.com>
2020-06-29 21:34:16 +0900

    Issue内のlabel項目の取得はしない実装に変更

--- source/Domain/Domain/Entity/IssueEntity.cs
+++ source/Domain/Domain/Entity/IssueEntity.cs
@@ -11,7 +11,6 @@ namespace Domain.Domain.Entity
     public int number { set; get; }
     public string title { set; get; }
     public UserEntity user { set; get; }
-    public List<string> labels { set; get; }
     public string state { set; get; }
     public DateTimeOffset created_at { set; get; }
     public DateTimeOffset updated_at { set; get; }
