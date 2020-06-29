44e3ccdfe495d3fe4bf67be90f6db3f25eca7e29
kazenetu <rakudafactory@gmail.com>
2020-06-29 21:42:03 +0900

    テスト用リポジトリクラスを追加

---
+++ source/Test/Repository/ApiRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Domain.Entity;
+using Domain.IntrastructureInterface;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Test.Repository
+{
+  class ApiRepository : IApiRepository
+  {
+    public string JsonText { set; get; }
+
+    public List<IssueEntity> GetLatestIssues()
+    {
+      return JsonSerializer.Deserialize<List<IssueEntity>>(JsonText);
+    }
+  }
+}
---
+++ source/Test/Repository/IssueRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Domain.Entity;
+using Domain.IntrastructureInterface;
+using System.Collections.Generic;
+
+namespace Test.Repository
+{
+  class IssueRepository : IIssueRepository
+  {
+    private List<IssueEntity> issues = new List<IssueEntity>();
+
+    public List<IssueEntity> GetIssues()
+    {
+      return issues;
+    }
+
+    public bool SetIssues(List<IssueEntity> target)
+    {
+      issues.Clear();
+      issues.AddRange(target);
+      return true;
+    }
+  }
+}
