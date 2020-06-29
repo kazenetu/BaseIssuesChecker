20c0cf06ff367b29d7fb40534bdbc8c03248608d
kazenetu <rakudafactory@gmail.com>
2020-06-29 21:39:57 +0900

    Issue用ドメインサービスを追加

---
+++ source/Domain/Domain/Service/IssuesService.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Domain.Entity;
+using Domain.IntrastructureInterface;
+using System.Collections.Generic;
+
+namespace Domain.Domain.Service
+{
+  /// <summary>
+  /// Issue用サービス
+  /// </summary>
+  public class IssuesService
+  {
+    public List<IssueEntity> GetIssues(IIssueRepository issueRepository,IApiRepository apiRepository)
+    {
+      var issuesEntity = IssuesEntity.Create(issueRepository.GetIssues());
+      var result = issuesEntity.GetModifyIssues(apiRepository.GetLatestIssues());
+      issueRepository.SetIssues(result);
+
+      return result;
+    }
+  }
+}
