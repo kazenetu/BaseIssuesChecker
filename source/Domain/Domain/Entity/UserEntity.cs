ebcbdacbe831124a82aeb6b93adf338a119677f0
kazenetu <rakudafactory@gmail.com>
2020-06-29 21:30:48 +0900

    書式フォーマット適用

--- source/Domain/Domain/Entity/UserEntity.cs
+++ source/Domain/Domain/Entity/UserEntity.cs
@@ -2,16 +2,19 @@ using System;
 
 namespace Domain.Domain.Entity
 {
-    public class UserEntity
-    {
-        public string login{set;get;}
-        public string email{set;get;}
-        public string type{set;get;}
-        public bool site_admin{set;get;}
-        public DateTimeOffset created_at{set;get;}
-        public int id{set;get;}
-        public string url{set;get;}
-        public string html_url{set;get;}
-        public string avatar_url{set;get;}
-    }
+  /// <summary>
+  /// ユーザーエンティティ
+  /// </summary>
+  public class UserEntity
+  {
+    public string login { set; get; }
+    public string email { set; get; }
+    public string type { set; get; }
+    public bool site_admin { set; get; }
+    public DateTimeOffset created_at { set; get; }
+    public int id { set; get; }
+    public string url { set; get; }
+    public string html_url { set; get; }
+    public string avatar_url { set; get; }
+  }
 }
