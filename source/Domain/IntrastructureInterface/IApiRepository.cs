56af0e537e1638aaf5a5bb5a68cbcefc37a13d4e
kazenetu <rakudafactory@gmail.com>
2020-06-29 21:38:51 +0900

    リポジトリインターフェイスの追加

---
+++ source/Domain/IntrastructureInterface/IApiRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Domain.IntrastructureInterface
+{
+  /// <summary>
+  /// API呼び出しインターフェイス
+  /// </summary>
+  public interface IApiRepository
+  {
+    List<IssueEntity> GetLatestIssues();
+  }
+}
---
+++ source/Domain/IntrastructureInterface/IIssueRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Domain.IntrastructureInterface
+{
+  /// <summary>
+  /// Issueリポジトリインターフェイス
+  /// </summary>
+  public interface IIssueRepository
+  {
+    List<IssueEntity> GetIssues();
+    bool SetIssues(List<IssueEntity> target);
+  }
+}
