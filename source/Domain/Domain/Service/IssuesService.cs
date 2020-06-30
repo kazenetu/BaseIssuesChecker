bf3f03d1cb69da9802bb2f17f5e04f3baa0a2799
kazenetu <rakudafactory@gmail.com>
2020-06-30 21:45:29 +0900

    クラススコープを変更

--- source/Domain/Domain/Service/IssuesService.cs
+++ source/Domain/Domain/Service/IssuesService.cs
@@ -7,8 +7,14 @@ namespace Domain.Domain.Service
   /// <summary>
   /// Issue用サービス
   /// </summary>
-  public class IssuesService
+  class IssuesService
   {
+    /// <summary>
+    /// APIから最新のIssue情報を取得、差分の情報を返す
+    /// </summary>
+    /// <param name="issueRepository">Issueリポジトリインスタンス</param>
+    /// <param name="apiRepository">API呼び出しリポジトリインスタンス</param>
+    /// <returns>最新差分Issue情報</returns>
     public List<IssueEntity> GetIssues(IIssueRepository issueRepository,IApiRepository apiRepository)
     {
       var issuesEntity = IssuesEntity.Create(issueRepository.GetIssues());
