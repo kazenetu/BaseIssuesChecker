c3642a82ae5ec251cb15cfb47cabb889432b77e4
kazenetu <rakudafactory@gmail.com>
2020-07-01 21:30:13 +0900

    コンソールアプリを追加

---
+++ source/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Application;
+using Intrastructure;
+using System;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // 第一パラメータにリポジトリのIssuesをJsonで取得するAPI(GET)のURIを指定する
+      if(args.Length < 1)
+      {
+        Console.WriteLine("Issue取得のURLを設定してください。");
+        return;
+      }
+
+      var issueRep = IssueRepository.GetInstance();
+      var apiRep = new ApiRepository(args[0]);
+      var app = new IssuesApplication();
+      var issues = app.GetIssues(issueRep, apiRep);
+
+      foreach(var issue in issues)
+      {
+        Console.WriteLine($"{issue.number} [{issue.body}]");
+      }
+
+    }
+  }
+}
